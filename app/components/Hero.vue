d807b16a5f043f6b86552850b794058a1832803d
notxorand <ewanretorokugbe@gmail.com>
2026-02-14 17:28:17 +0100

    fix: hero button misalignment

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -30,7 +30,7 @@
                             class="col-[1/3] xl:col-[1/4] flex items-center gap-12"
                         >
                             <div
-                                class="w-full max-w-[800px] gap-6 sm:gap-12 flex flex-col sm:flex-row"
+                                class="w-full max-w-[800px] gap-6 lg:gap-12 flex flex-col lg:flex-row"
                             >
                                 <nuxt-link
                                     class="h-[36px] no-select px-8 text-center bg-[#dcb] relative flex justify-center items-center link-vertical"
