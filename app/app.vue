5692865e0a93286cf00ac0a6371cb19649627759
notxorand <ewanretorokugbe@gmail.com>
2026-02-14 17:28:32 +0100

    chore: data analytics

--- app/app.vue
+++ app/app.vue
@@ -93,3 +93,20 @@ onMounted(() => {
   }
 }
 </style> -->
+
+<script setup>
+useHead({
+    script: [
+        {
+            src: "https://cdn.databuddy.cc/databuddy.js",
+            async: true,
+            crossorigin: "anonymous",
+            "data-client-id": "eb139f28-6f26-4253-924d-a75fbf37fa85",
+            "data-track-outgoing-links": "true",
+            "data-track-interactions": "true",
+            "data-track-scroll-depth": "true",
+            "data-track-web-vitals": "true",
+        },
+    ],
+});
+</script>
